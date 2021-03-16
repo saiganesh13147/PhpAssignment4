f4c0f8944f0cd791efdafdf3ed697fe0dbcd26ad
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2021-03-16 02:21:35 -0400

    Added input.php

---
+++ input.php
@@ -0,0 +1,52 @@
+<?php
+
+if($_SERVER['METHOD'] === 'POST'){
+
+    if(empty($_POST['username'])){
+        
+        echo "Please enter User Name<br>";
+        
+    }else{
+        
+        $username = $_POST['username'];
+        
+    }
+    if(empty($_POST['message'])){
+
+        echo "Please enter Message<br>";
+
+    }
+    else{
+
+        $message = $_POST['message'];
+    }
+
+    $query = "INSERT into Messages (username,message) values (?,?)";
+
+    $stmt = $mysqli->prepare($query);
+
+    $stmt->bind_param("ss", $username, $message );
+
+    $stmt->execute();
+
+    $stmt->store_result();
+
+
+    if ($mysqli->error) { 
+
+        echo $mysqli->error . "<br>";
+        
+        echo "Failure <br>";
+
+
+        } 
+        else { 
+            
+
+        echo "Success <br>";
+        
+    }
+     
+
+}
+?>
