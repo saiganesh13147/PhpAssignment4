fcceddb2bc1e21aa1b4981c9f5bc219d65ce468b
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2021-03-22 20:34:42 -0400

    added folders

---
+++ Part1/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div class='container'>
+    <br>
+    <br>
+    <form method='post' action='action.php'>
+    <div class="form-group"> 
+    <input type='button' value='chocalate'>
+    <!-- </div>
+    <div class='form-group'> -->
+    <input type='button' value='pop'> 
+    <!-- </div>
+    <div class='form-group'> -->
+    <input type='button' value='chips'> 
+    <!-- </div> -->
+    <br>
+    <br>
+    <div class='form-group'>
+    <button name='dollar' id='dollar'>1 dollar</button>
+    <button name='fivecents' id='fivecents'>25 cents</button>
+    <button name='tencents' id='tencents'>10 cents</button>
+    <button name='fivecents' id='fivecents'>5 cents</button><br><br>
+    </div>
+    <br>
+    <br>
+    <input type='submit' value='submit'>
+    </form>
+    
+</div>
+
+  </body>
+</html>
--- Part2/ajax.sql
+++ Part2/ajax.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE Message_Board;
+CREATE DATABASE Message_Board_SaiGanesh;
 
-USE Message_Board;
+USE Message_Board_SaiGanesh;
 
 CREATE TABLE Messages (
 username varchar(30) PRIMARY KEY,
